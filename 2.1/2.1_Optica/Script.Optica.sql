fe87dbe1731a34db5c6d98e15766533aae3fea8c
Ivan <Ivandgea@gmail.com>
2023-05-23 16:18:15 +0200

    imagenes añadidas

--- 2.1/2.1_Optica/Script.Optica.sql
+++ 2.1/2.1_Optica/Script.Optica.sql
@@ -1,55 +1,40 @@
-CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`CLIENTS` (
-  `idClients` INT NOT NULL,
-  `nom` VARCHAR(45) NULL,
-  `adreça_postal` VARCHAR(45) NULL,
-  `telefon` VARCHAR(45) NULL,
-  `correu_electronic` VARCHAR(45) NULL,
-  `data_registre` DATE NULL,
-  `client_recomanador_id` INT NULL,
-  PRIMARY KEY (`idClients`),
-  INDEX `client_recomanador_id_idx` (`client_recomanador_id` ASC) VISIBLE,
-  CONSTRAINT `client_recomanador_id`
-    FOREIGN KEY (`client_recomanador_id`)
-    REFERENCES `Cul_d'ampolla`.`CLIENTS` (`idClients`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+CREATE SCHEMA IF NOT EXISTS `Cul_d'ampolla` DEFAULT CHARACTER SET utf8 ;
+USE `Cul_d'ampolla` ;
 
-CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`Empleats` (
-  `idEmpleats` INT NOT NULL,
-  `nom` VARCHAR(45) NULL,
-  `cognoms` VARCHAR(45) NULL,
-  PRIMARY KEY (`idEmpleats`))
-ENGINE = InnoDB;
-
+-- -----------------------------------------------------
+-- Table `Cul_d'ampolla`.`PROVEÏDOR`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`PROVEÏDOR` (
-  `idPROVEEDORES` INT NOT NULL,
-  `nom` VARCHAR(45) NULL,
-  `adreça_carrer` VARCHAR(45) NULL,
-  `adreça_numero` VARCHAR(4) NULL,
+  `idPROVEEDORES` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `adreça_carrer` VARCHAR(45) NOT NULL,
+  `adreça_numero` VARCHAR(4) NOT NULL,
   `adreça_pis` VARCHAR(2) NULL,
   `adreça_porta` VARCHAR(2) NULL,
-  `ciutat` VARCHAR(20) NULL,
-  `codi_postal` VARCHAR(10) NULL,
-  `país` VARCHAR(45) NULL,
-  `telefon` VARCHAR(45) NULL,
+  `ciutat` VARCHAR(20) NOT NULL,
+  `codi_postal` VARCHAR(10) NOT NULL,
+  `país` VARCHAR(45) NOT NULL,
+  `telefon` VARCHAR(45) NOT NULL,
   `fax` VARCHAR(45) NULL,
-  `NIF` VARCHAR(45) NULL,
-  `PROVEÏDORcol` VARCHAR(45) NULL,
+  `NIF` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idPROVEEDORES`))
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `Cul_d'ampolla`.`ULLERES`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`ULLERES` (
-  `idULLERES` INT NOT NULL,
-  `marca` VARCHAR(45) NULL,
-  `ProveidorId` INT NULL,
-  `Preu` DECIMAL(10,2) NULL,
-  `graduacio_vidre_dret` FLOAT NULL,
-  `graduacio_vidre_esquerra` FLOAT NULL,
-  `muntura` VARCHAR(45) NULL,
-  `color_muntura` VARCHAR(45) NULL,
-  `color_vidre_dret` VARCHAR(45) NULL,
-  `color_vidre_esquerra` VARCHAR(45) NULL,
+  `idULLERES` INT NOT NULL AUTO_INCREMENT,
+  `marca` VARCHAR(45) NOT NULL,
+  `ProveidorId` INT NOT NULL,
+  `Preu` DECIMAL(10,2) NOT NULL,
+  `graduacio_vidre_dret` FLOAT NOT NULL,
+  `graduacio_vidre_esquerra` FLOAT NOT NULL,
+  `muntura` VARCHAR(45) NOT NULL,
+  `color_muntura` VARCHAR(45) NOT NULL,
+  `color_vidre_dret` VARCHAR(45) NOT NULL,
+  `color_vidre_esquerra` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idULLERES`),
   INDEX `ProveidorId_idx` (`ProveidorId` ASC) VISIBLE,
   CONSTRAINT `ProveidorId`
@@ -59,12 +44,48 @@ CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`ULLERES` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `Cul_d'ampolla`.`CLIENTS`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`CLIENTS` (
+  `idClients` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `adreça_postal` VARCHAR(45) NOT NULL,
+  `telefon` VARCHAR(45) NOT NULL,
+  `correu_electronic` VARCHAR(45) NOT NULL,
+  `data_registre` DATE NOT NULL,
+  `client_recomanador_id` INT NOT NULL,
+  PRIMARY KEY (`idClients`),
+  INDEX `client_recomanador_id_idx` (`client_recomanador_id` ASC) VISIBLE,
+  CONSTRAINT `client_recomanador_id`
+    FOREIGN KEY (`client_recomanador_id`)
+    REFERENCES `Cul_d'ampolla`.`CLIENTS` (`idClients`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Cul_d'ampolla`.`Empleats`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`Empleats` (
+  `idEmpleats` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NOT NULL,
+  `cognoms` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idEmpleats`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `Cul_d'ampolla`.`VENDES`
+-- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `Cul_d'ampolla`.`VENDES` (
-  `idVendes` INT NOT NULL,
-  `client_id` INT NULL,
-  `ulleres_id` INT NULL,
-  `empleat_id` INT NULL,
-  `data` DATE NULL,
+  `idVendes` INT NOT NULL AUTO_INCREMENT,
+  `client_id` INT NOT NULL,
+  `ulleres_id` INT NOT NULL,
+  `empleat_id` INT NOT NULL,
+  `data` DATE NOT NULL,
   PRIMARY KEY (`idVendes`),
   INDEX `client_id_idx` (`client_id` ASC) VISIBLE,
   INDEX `ulleres_id_idx` (`ulleres_id` ASC) VISIBLE,
